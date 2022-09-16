06f0564cd1087f48a9154a8a3d9ac4dc3715c596
Sara <saraeliza.lirio@gmail.com>
2022-09-16 14:35:54 -0300

    🚧 feat: Adicionando caminho das rotas CRUD Produtos

---
+++ src/routes/routerProdutos.js
@@ -0,0 +1,16 @@
+import produtosController from "../controller/produtosController"
+
+const routerProdutos = (app)=> {
+
+    app.get('/produtos', produtosController.menuProdutos)
+    app.get('/produtos/nomeProduto/:nomeProduto', produtosController.buscaProduto)
+    app.get('/produtos/idProduto/:idProduto', produtosController.buscaProdutoId)
+
+    app.post('/produtos', produtosController.cadastroNovoProduto)
+
+    app.delete('/produtos/idProduto/:idProduto', produtosController.removeProduto)
+
+    app.put('/produtos/idProduto/:idProduto', produtosController.atualizaProduto)
+}
+
+export default routerProdutos
