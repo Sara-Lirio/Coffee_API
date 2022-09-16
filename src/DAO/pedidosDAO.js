22812db3c24f80b06ba4a29733cd3a3d2800b429
Sara <saraeliza.lirio@gmail.com>
2022-09-16 12:13:30 -0300

    ✨ feat: Incluindo CREATE/INSERT em Pedidos

--- src/DAO/pedidosDAO.js
+++ src/DAO/pedidosDAO.js
@@ -1,5 +1,22 @@
 const daoPedidos = {
 
+    inserePedido: (pedido) => {
+        const INSERE_PEDIDO = `
+        INSERT INTO PEDIDOS (dataPedido, nomeCliente, cpfCliente, formaDeEntrega, valorTotal, statusPagamento, statusPedido)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        `
+        return new Promise((resolve, reject) => {
+            db.run(INSERE_PEDIDO, pedido.dataPedido, pedido.nomeCliente, pedido.cpfCliente,
+                pedido.formaDeEntrega, pedido.valorTotal, pedido.statusPagamento, pedido.statusPedido,
+                (error) => {
+                    if (error)
+                        reject(error)
+                    else
+                        resolve(pedido)
+                })
+        })
+    },
+
     pegaTodosPedidos: () => {
         const PEGAR_TODOS_PEDIDOS = `
         SELECT * FROM PEDIDOS
@@ -44,28 +61,28 @@ const daoPedidos = {
         })
     },
 
-    pegaPedidoPeloCPF : (cpfCliente)=> {
+    pegaPedidoPeloCPF: (cpfCliente) => {
         const PEGA_PEDIDO_CPF = `
         SELECT * FROM PEDIDOS
         WHERE cpfCliente = ?
         `
-        return new Promise((resolve, reject)=> {
-            db.get(PEGA_PEDIDO_CPF, cpfCliente, (error, row)=> {
-                if (error) 
+        return new Promise((resolve, reject) => {
+            db.get(PEGA_PEDIDO_CPF, cpfCliente, (error, row) => {
+                if (error)
                     reject(error)
-                else 
+                else
                     resolve(row)
             })
         })
     },
 
-    pegaPedidoPeloId : (id)=> {
+    pegaPedidoPeloId: (id) => {
         const PEGA_PEDIDO_ID = `
         SELECT * FROM PEDIDOS
         WHERE id = ?
         `
-        return new Promise((resolve, reject)=> {
-            db.get(PEGA_PEDIDO_ID, id, (error, row)=> {
+        return new Promise((resolve, reject) => {
+            db.get(PEGA_PEDIDO_ID, id, (error, row) => {
                 if (error)
                     reject(error)
                 else
