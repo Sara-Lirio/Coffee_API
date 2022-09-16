6560223ff11fbf5b53693ae01ab38f7c38c87d45
Sara <saraeliza.lirio@gmail.com>
2022-09-16 13:17:29 -0300

    ✨ feat: Incluindo rotas de GET/READ

--- src/controller/pedidoController.js
+++ src/controller/pedidoController.js
@@ -31,6 +31,126 @@ const pedidoController = {
         }
       },
 
+      buscarPedidos: async (req, res) => {
+        try {
+          const resposta = await modelPedido.buscarTodosPedidos();
+    
+          if (resposta.status === 200) {
+            res.status(resposta.status).json({
+              pedidos: resposta.dados,
+              erro: false,
+            });
+          } else {
+            res.status(resposta.status).json({
+              msg: resposta.mensagem,
+              erro: true,
+            });
+          }
+        } catch (error) {
+          res.status(400).json({
+            msg: error.message,
+            erro: true,
+          });
+        }
+      },
+
+      buscarPedidoData: async (req, res) => {
+        const dataPedido = req.params.dataPedido;
+        try {
+          const resposta = await modelPedido.buscarPedidoData(dataPedido);
+    
+          if (resposta.status === 200) {
+            res.status(resposta.status).json({
+              pedido: resposta.dados,
+              erro: false,
+            });
+          } else {
+            res.status(resposta.status).json({
+              msg: resposta.mensagem,
+              erro: true,
+            });
+          }
+        } catch (error) {
+          res.status(400).json({
+            msg: error.message,
+            erro: true,
+          });
+        }
+      },
+
+      buscarPedidoPeloNome: async (req, res) => {
+        const nomeCliente = req.params.nomeCliente;
+        try {
+          const resposta = await modelPedido.buscarPedidoNome(nomeCliente);
+    
+          if (resposta.status === 200) {
+            res.status(resposta.status).json({
+              pedido: resposta.dados,
+              erro: false,
+            });
+          } else {
+            res.status(resposta.status).json({
+              msg: resposta.mensagem,
+              erro: true,
+            });
+          }
+        } catch (error) {
+          res.status(400).json({
+            msg: error.message,
+            erro: true,
+          });
+        }
+      },
+    
+      buscarPedidoPeloCPF: async (req, res) => {
+        const cpfCliente = req.params.cpfCliente;
+    
+        try {
+          const resposta = await modelPedido.buscarPedidoCPF(cpfCliente);
+    
+          if (resposta.status === 200) {
+            res.status(resposta.status).json({
+              pedido: resposta.dados,
+              erro: false,
+            });
+          } else {
+            res.status(resposta.status).json({
+              msg: resposta.mensagem,
+              erro: true,
+            });
+          }
+        } catch (error) {
+          res.status(400).json({
+            msg: error.message,
+            erro: true,
+          });
+        }
+      },
+    
+      buscarPedidoPeloID: async (req, res) => {
+        const id = req.params.id;
+        try {
+          const resposta = await modelPedido.buscarPedidoId(id);
+    
+          if (resposta.status === 200) {
+            res.status(resposta.status).json({
+              pedido: resposta.dados,
+              erro: false,
+            });
+          } else {
+            res.status(resposta.status).json({
+              msg: resposta.mensagem,
+              erro: true,
+            });
+          }
+        } catch (error) {
+          res.status(400).json({
+            msg: error.message,
+            erro: true,
+          });
+        }
+      },
+    
 }
 
 export default pedidoController
