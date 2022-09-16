680ca672896bec78b33655e8f9b2a18c81b20966
Sara <saraeliza.lirio@gmail.com>
2022-09-16 14:37:33 -0300

    ✨ feat: importando rotas em app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from "express"
 import cors from "cors"
+import routerPedidos from "./routes/routerPedidos"
+import routerProdutos from "./routes/routerProdutos"
 
 const app = express ()
 
 app.use(cors())
 app.use(express.json())
+
+routerPedidos(app)
+routerProdutos(app)
 
 const port = process.env.PORT || 3000;
 
