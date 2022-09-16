3ca849f16dd45e356a5f40ec2233ce82d93160f6
Sara <saraeliza.lirio@gmail.com>
2022-09-16 13:34:07 -0300

    ✨ feat: Incluindo método de Inserir/Adicionar o Produto

--- src/model/produtosModel.js
+++ src/model/produtosModel.js
@@ -1,5 +1,22 @@
+import dao from '../DAO/produtosDAO.js'
+
 class Produtos {
-    constructor (nomeProduto, imagem, descricao, valor, adicional)
+
+    cadastroProduto = async (nomeProduto, imagem, descricao, valor, adicional) =>{
+        try {
+            const produto = this.criaProduto(nomeProduto, imagem, descricao, valor, adicional)
+            const data = await dao.cadastrarProduto(produto)
+            return {
+                "dados": data,
+                "status": 200
+            }
+        } catch (error) {
+            return {
+                "mensagem": error.message,
+                "status": 400
+            }
+        }
+    }
 }
 
 export default Produtos
