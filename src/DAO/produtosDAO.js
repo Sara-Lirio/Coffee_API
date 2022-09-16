865f3f0291bdd66690e907b9d5731da76f589594
Sara <saraeliza.lirio@gmail.com>
2022-09-16 13:31:23 -0300

    ✨ feat: Incluindo CREATE/INSERT em Produtos

--- src/DAO/produtosDAO.js
+++ src/DAO/produtosDAO.js
@@ -1,4 +1,25 @@
 const daoProdutos = {
+    cadastrarProduto: (produto)=>{
+        const ADD_PRODUTO = `
+        INSERT INTO PRODUTOS (nomeProduto, imagem, descricao, valor, adicional)
+        VALUES (?, ?, ?, ?, ?)
+        `
+        return new Promise((resolve, reject)=>{
+            db.run(ADD_PRODUTO, 
+                produto.nomeProduto, 
+                produto.imagem, 
+                produto.descricao, 
+                produto.valor, 
+                produto.adicional,
+                (error)=>{
+                    if(error)
+                        reject(error)
+                    else
+                        resolve(produto)
+                }
+            )
+        })
+    },
 
 }
 
