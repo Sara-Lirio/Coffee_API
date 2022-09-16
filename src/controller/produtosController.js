b7df187b04ba969750465139c1d3417ae8b9199d
Sara <saraeliza.lirio@gmail.com>
2022-09-16 14:11:44 -0300

    feat: Incluindo rota de CREATE em Produtos

--- src/controller/produtosController.js
+++ src/controller/produtosController.js
@@ -3,7 +3,30 @@ import Produtos from "../model/produtosModel"
 const modelProdutos = new Produtos()
 
 const produtosController = {
-
+    cadastroNovoProduto: async (req, res) => {
+        const body = req.body;
+    
+        try {
+          const resposta = await modelProdutos.cadastroProduto(
+            body.nomeProduto,
+            body.imagem, 
+            body.descricao, 
+            body.valor,
+            body.adicional
+          );
+    
+          res.status(resposta.status).json({
+            msg: 'Cadastro realizado com sucesso',
+            bebida: resposta.dados,
+            erro: false,
+          });
+        } catch (error) {
+          res.status(resposta.status).json({
+            msg: error.message,
+            erro: true,
+          });
+        }
+      },
 }
 
 export default produtosController
