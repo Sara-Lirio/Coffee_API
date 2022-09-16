cd1a538543d81bc57f105377e6376b3749d4d76f
Sara <saraeliza.lirio@gmail.com>
2022-09-16 12:37:48 -0300

    ✨ feat: Removendo UPTADE pelo Nome e CPF do Cliente

--- src/DAO/pedidosDAO.js
+++ src/DAO/pedidosDAO.js
@@ -112,48 +112,6 @@ const daoPedidos = {
         })
     },
 
-    atualizarPedidoNome: (nomeCliente, novoPedido) => {
-        const ATUALIZA_PEDIDO_NOME = `
-        UPDATE PEDIDOS 
-        SET dataPedido = ? , nomeCliente = ? , cpfCliente = ?, formaDeEntrega = ? ,
-        valorTotal = ? , statusPagamento = ? , statusPedido = ? 
-        WHERE nomeCliente = ?
-        `
-        return new Promise((resolve, reject) => {
-            db.run(ATUALIZA_PEDIDO_NOME,
-                novoPedido.dataPedido, novoPedido.nomeCliente, novoPedido.cpfCliente, novoPedido.formaDeEntrega,
-                novoPedido.valorTotal, novoPedido.statusPagamento, novoPedido.statusPedido,
-                nomeCliente,
-                (error) => {
-                    if (error)
-                        reject(error)
-                    else
-                        resolve(novoPedido)
-                })
-        })
-    },
-
-    atualizarPedidoCPF: (cpfCliente, novoPedido) => {
-        const ATUALIZA_PEDIDO_CPF = `
-        UPDATE PEDIDOS 
-        SET dataPedido = ? , nomeCliente = ? , cpfCliente = ?, formaDeEntrega = ? ,
-        valorTotal = ? , statusPagamento = ? , statusPedido = ? 
-        WHERE cpfCliente = ?
-        `
-        return new Promise((resolve, reject) => {
-            db.run(ATUALIZA_PEDIDO_CPF,
-                novoPedido.dataPedido, novoPedido.nomeCliente, novoPedido.cpfCliente, novoPedido.formaDeEntrega,
-                novoPedido.valorTotal, novoPedido.statusPagamento, novoPedido.statusPedido,
-                cpfCliente,
-                (error) => {
-                    if (error)
-                        reject(error)
-                    else
-                        resolve(novoPedido)
-                })
-        })
-    },
-
 }
 
 export default daoPedidos
