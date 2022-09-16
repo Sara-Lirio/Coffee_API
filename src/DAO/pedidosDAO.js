c7a55b224b60a5a17c2b4a1a50b87b27f2d18a5d
Sara <saraeliza.lirio@gmail.com>
2022-09-16 11:49:19 -0300

    :sparkles: feat: Incluindo READ/SELECT por nomeCliente

--- src/DAO/pedidosDAO.js
+++ src/DAO/pedidosDAO.js
@@ -1,18 +1,32 @@
 const daoPedidos = {
 
-    pegaTodosPedidos : ()=> {
-    const PEGAR_TODOS_PEDIDOS = `
-    SELECT * FROM PEDIDOS
-    `
-    return new Promise((resolve, reject)=> {
-        db.all(PEGAR_TODOS_PEDIDOS, (error, row)=> {
-            if (error)
-                reject(error)
-            else
-                resolve(row)
+    pegaTodosPedidos: () => {
+        const PEGAR_TODOS_PEDIDOS = `
+        SELECT * FROM PEDIDOS
+        `
+        return new Promise((resolve, reject) => {
+            db.all(PEGAR_TODOS_PEDIDOS, (error, row) => {
+                if (error)
+                    reject(error)
+                else
+                    resolve(row)
+            })
         })
-    })
+    },
 
+    pegaPedidoPeloNome: (nomeCliente) => {
+        const PEGA_PEDIDO_NOME = `
+        SELECT * FROM PEDIDOS 
+        WHERE nomeCliente = ?
+        `
+        return new Promise((resolve, reject) => {
+            db.get(PEGA_PEDIDO_NOME, nomeCliente, (error, row) => {
+                if (error)
+                    reject(error)
+                else
+                    resolve(row)
+            })
+        })
     },
 
 
