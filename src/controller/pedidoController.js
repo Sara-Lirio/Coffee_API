41898cf7cb0941d46bf83c88309cb7ba672b08f4
Sara <saraeliza.lirio@gmail.com>
2022-09-16 13:13:49 -0300

    ✨ feat: Incluindo rota de CREATE

--- src/controller/pedidoController.js
+++ src/controller/pedidoController.js
@@ -4,6 +4,33 @@ const modelPedido = new Pedido ()
 
 const pedidoController = {
 
+    criarNovoPedido: async (req, res) => {
+        const body = req.body;
+    
+        try {
+          const resposta = await modelPedido.inserirPedido(
+            body.dataPedido,
+            body.nomeCliente, 
+            body.cpfCliente,
+            body.formaDeEntrega, 
+            body.valorTotal,
+            body.statusPagamento,
+            body.statusPedido
+          );
+          if (resposta.status != 200) throw resposta;
+          res.status(resposta.status).json({
+            msg: 'Pedido inserido com sucesso',
+            pedido: resposta.dados,
+            erro: false,
+          });
+        } catch (error) {
+          res.status(400).json({
+            msg: error.mensagem,
+            erro: true,
+          });
+        }
+      },
+
 }
 
 export default pedidoController
