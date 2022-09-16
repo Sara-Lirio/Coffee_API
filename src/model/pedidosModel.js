a24a7635127fd7aaaada1bc2268abb6aaa1ce4d6
Sara <saraeliza.lirio@gmail.com>
2022-09-16 12:03:35 -0300

    ✨ feat: Incluindo método Buscar Pedidos pela Data do Pedido

--- src/model/pedidosModel.js
+++ src/model/pedidosModel.js
@@ -1,25 +1,45 @@
 import dao from '../DAO/pedidosDAO.js'
 class Pedido {
-    constructor (dataPedido, nomeCliente, cpfCliente, formaDeEntrega, valorTotal, statusPagamento, statusPedido)
+    constructor(dataPedido, nomeCliente, cpfCliente, formaDeEntrega, valorTotal, statusPagamento, statusPedido)
 
-    
+
     buscarTodosPedidos = async () => {
         try {
-          const data = await dao.pegaTodosPedidos();
-          return {
-            dados: data,
-            tamanho: data.length,
-            status: 200,
-          };
+            const data = await dao.pegaTodosPedidos();
+            return {
+                dados: data,
+                tamanho: data.length,
+                status: 200,
+            };
         } catch (error) {
-          return {
-            mensagem: error.message,
-            status: 400,
-          };
+            return {
+                mensagem: error.message,
+                status: 400,
+            };
         }
-      };
-    
+    };
 
+    buscarPedidoData = async (dataPedido) => {
+        try {
+            const data = await dao.pegaPedidoPelaData(dataPedido);
+            if (data) {
+                return {
+                    dados: data,
+                    status: 200,
+                };
+            } else {
+                return {
+                    mensagem: `Não foi encontrado nenhum pedido na data: ${dataPedido}`,
+                    status: 404,
+                };
+            }
+        } catch (error) {
+            return {
+                mensagem: error.message,
+                status: 400,
+            };
+        }
+    };
 
 }
 
