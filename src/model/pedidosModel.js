618c23ca3f1929026b872c567932747d1ada5903
Sara <saraeliza.lirio@gmail.com>
2022-09-16 12:00:24 -0300

    ✨ feat: Incluindo método Buscar por Todos os Pedidos

--- src/model/pedidosModel.js
+++ src/model/pedidosModel.js
@@ -1,6 +1,26 @@
 import dao from '../DAO/pedidosDAO.js'
 class Pedido {
     constructor (dataPedido, nomeCliente, cpfCliente, formaDeEntrega, valorTotal, statusPagamento, statusPedido)
+
+    
+    buscarTodosPedidos = async () => {
+        try {
+          const data = await dao.pegaTodosPedidos();
+          return {
+            dados: data,
+            tamanho: data.length,
+            status: 200,
+          };
+        } catch (error) {
+          return {
+            mensagem: error.message,
+            status: 400,
+          };
+        }
+      };
+    
+
+
 }
 
 export default Pedido
