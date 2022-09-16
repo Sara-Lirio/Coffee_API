2a7ab7238d7a38b992f64fa4a01b9ce90af70545
Sara <saraeliza.lirio@gmail.com>
2022-09-16 13:39:27 -0300

    ✨ feat: Incluindo READ/SELECT em Produto

--- src/DAO/produtosDAO.js
+++ src/DAO/produtosDAO.js
@@ -1,18 +1,18 @@
 const daoProdutos = {
-    cadastrarProduto: (produto)=>{
+    cadastrarProduto: (produto) => {
         const ADD_PRODUTO = `
         INSERT INTO PRODUTOS (nomeProduto, imagem, descricao, valor, adicional)
         VALUES (?, ?, ?, ?, ?)
         `
-        return new Promise((resolve, reject)=>{
-            db.run(ADD_PRODUTO, 
-                produto.nomeProduto, 
-                produto.imagem, 
-                produto.descricao, 
-                produto.valor, 
+        return new Promise((resolve, reject) => {
+            db.run(ADD_PRODUTO,
+                produto.nomeProduto,
+                produto.imagem,
+                produto.descricao,
+                produto.valor,
                 produto.adicional,
-                (error)=>{
-                    if(error)
+                (error) => {
+                    if (error)
                         reject(error)
                     else
                         resolve(produto)
@@ -21,6 +21,49 @@ const daoProdutos = {
         })
     },
 
+    menuProdutos: () => {
+        const MENU_PRODUTOS = `
+        SELECT * FROM PRODUTOS
+        `
+        return new Promise((resolve, reject) => {
+            db.all(MENU_PRODUTOS, (error, row) => {
+                if (error)
+                    reject(error)
+                else
+                    resolve(row)
+            })
+        })
+    },
+
+    buscaProduto: (nomeProduto) => {
+        const BUSCA_PRODUTO = `
+        SELECT * FROM PRODUTOS
+        WHERE nomeProduto = ?
+        `
+        return new Promise((resolve, reject) => {
+            db.get(BUSCA_PRODUTO, nomeProduto, (error, row) => {
+                if (error)
+                    reject(error)
+                else
+                    resolve(row)
+            })
+        })
+    },
+
+    buscaProdutoId: (idProduto) => {
+        const BUSCA_PRODUTOID = `
+        SELECT * FROM PRODUTOS
+        WHERE idProduto = ?
+        `
+        return new Promise((resolve, reject) => {
+            db.get(BUSCA_PRODUTOID, idProduto, (error, row) => {
+                if (error)
+                    reject(error)
+                else
+                    resolve(row)
+            })
+        })
+    },
 }
 
 export default daoProdutos
